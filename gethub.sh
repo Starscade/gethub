341ac73e2371c3d7144e9bbd9508647401932626
Starscade <angus@starsca.de>
2026-04-24 10:28:01 -0700

    Handle errors on a case by case basis. Added box drawing character to messages.

--- gethub.sh
+++ gethub.sh
@@ -17,26 +17,38 @@ fi
 
 printf "\n"
 printf " \033[1;93mPACKAGE\033[0m: ${GETHUB_REPO_NAME}\n\n"
-printf " Fetching remote environment ...\n"
+printf " ┃ Fetching remote environment ...\n"
 
 wget -qO \
 	"$GETHUB_TMP_ENVIRONMENT_FILE" \
-	"https://${GETHUB_REPO_HOST}/${GETHUB_REPO_NAME}/${GETHUB_REPO_BRANCH}/${GETHUB_REPO_ENV}"
+	"https://${GETHUB_REPO_HOST}/${GETHUB_REPO_NAME}/${GETHUB_REPO_BRANCH}/${GETHUB_REPO_ENV}" \
+	|| {
+		printf " ┃ Couldn't find a valid GEThub environment.\n"
+		printf "\n \033[1;31mFAILED\033[0m\n\n"
+		exit 1
+	}
 
 export $(grep '^GETHUB_' "$GETHUB_TMP_ENVIRONMENT_FILE" | xargs)
 
-printf " Downloading executable ...\n"
+printf " ┃ Downloading executable ...\n"
 
 wget -qO \
 	"$GETHUB_TMP_EXECUTABLE_FILE" \
-	"https://${GETHUB_REPO_HOST}/${GETHUB_REPO_NAME}/${GETHUB_REPO_BRANCH}/${GETHUB_SOURCE_PATH}"
+	"https://${GETHUB_REPO_HOST}/${GETHUB_REPO_NAME}/${GETHUB_REPO_BRANCH}/${GETHUB_SOURCE_PATH}" \
+	|| {
+		printf " ┃ Couldn't find the specified source distributable.\n"
+		printf "\n \033[1;31mFAILED\033[0m\n\n"
+		exit 1
+	}
 
-printf " Installing ...\n"
+printf " ┃ Installing ...\n"
 
-cp "$GETHUB_TMP_EXECUTABLE_FILE" "${GETHUB_BIN_DIR}/${GETHUB_TARGET_NAME}"
+cp "$GETHUB_TMP_EXECUTABLE_FILE" "${GETHUB_BIN_DIR}/${GETHUB_TARGET_NAME}" \
+	|| printf " ┃ Failed to install.\n"
 
 chmod +x "${GETHUB_BIN_DIR}/${GETHUB_TARGET_NAME}"
 
-rm "$GETHUB_TMP_EXECUTABLE_FILE" "$GETHUB_TMP_ENVIRONMENT_FILE"
+rm "$GETHUB_TMP_EXECUTABLE_FILE" "$GETHUB_TMP_ENVIRONMENT_FILE" \
+	|| printf " ┃ Failed to purge temporary files. Perhaps they were removed already?\n"
 
 printf "\n \033[1;32mOK\033[0m\n\n"
